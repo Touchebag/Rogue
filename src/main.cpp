cb6557cce93346ba74b7ff13f2a7ee46cf326dbe
Touchebag <someone90_6@hotmail.com>
2013-05-30 20:38:44 +0200

    Removed debugging info

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main(int argc, char **argv){
 
-	 /* Initialize the library */
+    /* Initialize the library */
     if (!glfwInit())
-        	return -1;
-        
-        if (!glfwOpenWindow(640, 480, 8, 8, 8, 0, 24, 0, GLFW_WINDOW))
         return -1;
         
-        while (glfwGetWindowParam(GLFW_OPENED))
+    if (!glfwOpenWindow(640, 480, 8, 8, 8, 0, 24, 0, GLFW_WINDOW))
+        return -1;
+        
+    while (glfwGetWindowParam(GLFW_OPENED))
     {
         /* Swap front and back buffers and process events */
         glfwSwapBuffers();
