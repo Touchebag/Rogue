dde7dbc513d7e6ccc3b1ca87e4d8d115af0b03c0
Andreas Arvidsson <andreas.arvidson@gmail.com>
2013-06-04 23:41:12 +0200

    Initializing GLEW.

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,22 @@ using namespace std;
 int main(int argc, char **argv){
 
     /* Initialize the library */
-    if (!glfwInit())
+    cout << "Initializing GLFW...";
+    if (glfwInit()) {
+        cout << "Success!\n";
+    } else {
+        cout << "Failure!\n";
         return -1;
-        
+    }
+
+    cout << "Initializing GLEW...";
+    if(glewInit) {
+        cout << "Success!\n";
+    } else {
+        cout << "Failure!\n";
+        return -1;
+    }
+
     if (!glfwOpenWindow(640, 480, 8, 8, 8, 0, 24, 0, GLFW_WINDOW))
         return -1;
         
